8a48e2596f77033895a4a8d9670d475e55161fd3
TRX <hectorquiroz@trixocom.com>
2025-10-01 21:07:24 -0300

    feat: Add __manifest__.py for Odoo 13.0

---
+++ trx_partner_razonsocial/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Partner - Razón Social',
+    'version': '13.0.1.0.0',
+    'category': 'Contact',
+    'summary': 'Agrega campo Razón Social al partner',
+    'description': """
+        Partner - Razón Social
+        ======================
+        
+        Este módulo agrega el campo 'Razón Social' al modelo res.partner.
+        
+        Características:
+        ----------------
+        * Campo 'razonsocial' en el formulario de contacto
+        * Disponible para empresas y contactos
+    """,
+    'author': 'Trixocom',
+    'website': 'https://github.com/trixocom',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
